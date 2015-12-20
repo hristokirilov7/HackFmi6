3bfcf223f1abfc473ee5766afc59f9f61ef716f2
george dimov <george dimov>
2015-12-20 15:49:32 +0200

    Latchko

---
+++ stop.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <math.h>
+using namespace std;
+
+int Motor1A = 17;
+int Motor1B = 18;
+int Motor1E = 27;
+
+int Motor2A = 13;
+int Motor2B = 19;
+int Motor2E = 26;
+
+void stop() {
+	digitalWrite(Motor1A, 0);
+	digitalWrite(Motor1B, 0);
+	digitalWrite(Motor1E, 0);
+
+	digitalWrite(Motor2A, 0);
+	digitalWrite(Motor2B, 0);
+	digitalWrite(Motor2E, 0);
+}
+
+int main() {
+	stop(); 
+	return 0; 
+}
