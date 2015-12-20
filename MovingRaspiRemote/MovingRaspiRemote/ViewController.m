bca64380a8ddbb1fd5b1c1073ec1c63687146dcd
llluchko <l.mladenov21@gmail.com>
2015-12-20 15:59:43 +0200

    remote test

--- MovingRaspiRemote/MovingRaspiRemote/ViewController.m
+++ MovingRaspiRemote/MovingRaspiRemote/ViewController.m
@@ -8,31 +8,89 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
 
 @end
 
 @implementation ViewController
 
-@synthesize port, host, connect, disconnect, status;
+
+
+//@synthesize port, host, connect, disconnect, status;
 
 #pragma mark - View controller lifecycle
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.forwardTextField.delegate = self;
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapOnView:)];
+    [self.view addGestureRecognizer:tap];
+    
+    CFReadStreamRef readStream;
+    CFWriteStreamRef writeStream;
+    CFStreamCreatePairWithSocketToHost(NULL, (CFStringRef)CFBridgingRetain(@"10.0.201.48"), [@"5011" intValue], &readStream, &writeStream);
+    _outputStream = (NSOutputStream *)CFBridgingRelease(writeStream);
+    
+    [_outputStream setDelegate:self];
+    
+    [_outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    
+    [_outputStream open];
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
     
-    [status setText:@"Not connected"];
+   // [status setText:@"Not connected"];
     
-    // Load user defaults
-    if ([[NSUserDefaults standardUserDefaults] objectForKey:@"com.aboudou.movingraspiremote.host"] != nil) {
-        [host setText:[[NSUserDefaults standardUserDefaults] objectForKey:@"com.aboudou.movingraspiremote.host"]];
-    }
-    if ([[NSUserDefaults standardUserDefaults] objectForKey:@"com.aboudou.movingraspiremote.port"] != nil) {
-        [port setText:[[NSUserDefaults standardUserDefaults] objectForKey:@"com.aboudou.movingraspiremote.port"]];
-    }
+    
+//    // Load user defaults
+//    if ([[NSUserDefaults standardUserDefaults] objectForKey:@"10.0.201.48"] != nil) {
+//        [host setText:[[NSUserDefaults standardUserDefaults] objectForKey:@"10.0.201.48"]];
+//    }
+//    if ([[NSUserDefaults standardUserDefaults] objectForKey:@"5011"] != nil) {
+//        [port setText:[[NSUserDefaults standardUserDefaults] objectForKey:@"5011"]];
+//    }
 
+}
+
+- (void)tapOnView:(UITapGestureRecognizer *)sender
+{
+    CGPoint location = [sender locationInView:self.view];
+    NSLog(@"Tap at %1.0f, %1.0f", location.x, location.y);
+}
+
+
+// TODO parse x and y
+
+-(void)textViewShouldReturn:(UITextField *)textField
+{
+    if ([textField.text isEqualToString:@""]){
+        return;
+    }
+    
+    NSString *response = textField.text;
+    
+    NSData *data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+    [_outputStream write:[data bytes] maxLength:[data length]];
+    
+    // Display this message.
+    //[helloEarthInputAlert show];
+    
+}
+- (IBAction)moveForward:(id)sender
+{
+    if ([self.forwardTextField.text isEqualToString:@""]){
+        return;
+    }
+    
+    NSString *response = self.forwardTextField.text;
+    
+    NSData *data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+    [_outputStream write:[data bytes] maxLength:[data length]];
 }
 
 
@@ -45,7 +103,7 @@
 #pragma mark - Actions
 
 - (IBAction)goForward:(id)sender {
-    NSString *response  = @"1";
+    NSString *response  = @"forward";
 	NSData *data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
 	[_outputStream write:[data bytes] maxLength:[data length]];
 }
@@ -79,64 +137,66 @@
 }
 
 
-- (IBAction)doConnect:(id)sender {
-    // Save user defaults
-    NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setObject:[host text] forKey:@"com.aboudou.movingraspiremote.host"];
-    [defaults setObject:[port text] forKey:@"com.aboudou.movingraspiremote.port"];
-    [defaults synchronize];
-    
-    [[self host] resignFirstResponder];
-    [[self port] resignFirstResponder];
-    
-    [self doDisconnect:nil];
-    [self initNetworkCommunication];
-    _connectInProgress = YES;
-    
-    [status setText:@"Connection in progress…"];
-    [self performSelectorInBackground:@selector(waitForConnection:) withObject:nil];
-}
 
 
-- (IBAction)doDisconnect:(id)sender {
-    [[self host] resignFirstResponder];
-    [[self port] resignFirstResponder];
+//- (IBAction)doConnect:(id)sender {
+//    // Save user defaults
+//    NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+//    [defaults setObject:[host text] forKey:@"10.0.201.48"];
+//    [defaults setObject:[port text] forKey:@"5011"];
+//    [defaults synchronize];
+//    
+//    [[self host] resignFirstResponder];
+//    [[self port] resignFirstResponder];
+//    
+//    [self doDisconnect:nil];
+//    [self initNetworkCommunication];
+//    _connectInProgress = YES;
+//    
+//    [status setText:@"Connection in progress…"];
+//    [self performSelectorInBackground:@selector(waitForConnection:) withObject:nil];
+//}
 
-    [_outputStream close];
-    
-    _connectInProgress = NO;
-    
-    [status setText:@"Not connected"];
-}
+
+//- (IBAction)doDisconnect:(id)sender {
+//    [[self host] resignFirstResponder];
+//    [[self port] resignFirstResponder];
+//
+//    [_outputStream close];
+//    
+//    _connectInProgress = NO;
+//    
+//    [status setText:@"Not connected"];
+//}
 
 #pragma mark - Misc
 
 
-- (void) waitForConnection:(id) sender {
-    @autoreleasepool {
-        while ([_outputStream streamStatus] != NSStreamStatusOpen && _connectInProgress) {
-            [status performSelectorOnMainThread:@selector(setText:) withObject:@"Connection in progress…" waitUntilDone:YES];
-        }
-        if (_connectInProgress) {
-            [status performSelectorOnMainThread:@selector(setText:) withObject:[NSString stringWithFormat:@"Connected to %@:%@", [self.host text], [self.port text]] waitUntilDone:YES];
-        } else {
-            [status performSelectorOnMainThread:@selector(setText:) withObject:@"Not connected" waitUntilDone:YES];
-        }
-    }
-}
+//- (void) waitForConnection:(id) sender {
+//    @autoreleasepool {
+//        while ([_outputStream streamStatus] != NSStreamStatusOpen && _connectInProgress) {
+//            [status performSelectorOnMainThread:@selector(setText:) withObject:@"Connection in progress…" waitUntilDone:YES];
+//        }
+//        if (_connectInProgress) {
+//            [status performSelectorOnMainThread:@selector(setText:) withObject:[NSString stringWithFormat:@"Connected to %@:%@", [self.host text], [self.port text]] waitUntilDone:YES];
+//        } else {
+//            [status performSelectorOnMainThread:@selector(setText:) withObject:@"Not connected" waitUntilDone:YES];
+//        }
+//    }
+//}
 
 
-- (void)initNetworkCommunication {
-    CFReadStreamRef readStream;
-    CFWriteStreamRef writeStream;
-    CFStreamCreatePairWithSocketToHost(NULL, (CFStringRef)CFBridgingRetain([self.host text]), [[self.port text] intValue], &readStream, &writeStream);
-    _outputStream = (NSOutputStream *)CFBridgingRelease(writeStream);
-    
-    [_outputStream setDelegate:self];
-    
-    [_outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-    
-    [_outputStream open];
-}
+//- (void)initNetworkCommunication {
+//    CFReadStreamRef readStream;
+//    CFWriteStreamRef writeStream;
+//    CFStreamCreatePairWithSocketToHost(NULL, (CFStringRef)CFBridgingRetain(@"10.0.201.48"), [@"5011" intValue], &readStream, &writeStream);
+//    _outputStream = (NSOutputStream *)CFBridgingRelease(writeStream);
+//    
+//    [_outputStream setDelegate:self];
+//    
+//    [_outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+//    
+//    [_outputStream open];
+//}
 
 @end
